9e673705b454c6913a47f00e78d283729305ab89
mateuszanella <mateusblk1@gmail.com>
2024-06-10 20:17:09 -0300

    Added basic practice files

---
+++ Prática/dccp_server.c
@@ -0,0 +1,68 @@
+// File: dccp_server.c
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <linux/dccp.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_DCCP, IPPROTO_DCCP)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connection accepted\n");
+
+    int bytes_read;
+    while ((bytes_read = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
+        buffer[bytes_read] = '\0';
+        printf("Message: %s\n", buffer);
+    }
+
+    if (bytes_read < 0) {
+        perror("read");
+    }
+
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
